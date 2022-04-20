4af35f0586e8bd64a6b4570ae08e2c84bef890fd
gregory_kucherenko <gregorkucherenko@gmail.com>
2022-04-20 13:53:53 +0300

    implement serialize to json function

--- TestTask.cpp
+++ TestTask.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
+#include <map>
 // #define PRINT(x) #x << ": " << x
 
 class ListNode
@@ -25,9 +26,40 @@ public:
 	ListNode* tail_;
 	int count_;
 
-	void serialize(std::ofstream& s) {}
+	void serialize(std::ofstream& file) 
+	{
+		std::map<ListNode*, int> listStructure;
+		ListNode* current = head_;
+		
+		for (int index = 0; current; current = current->next_, ++index) 
+		{
+			listStructure.insert({ current, index });
+		}
 
-	void deserialize(std::ifstream& s) {}
+		file << "{\n";
+		current = head_;
+		int index = 0;
+		while (current)
+		{
+			const auto it = listStructure.find(current->rand_);
+			const int randElemIndex = it == listStructure.end() ? -1 : it->second;
+			file << getFormattedString(index, current->data_, randElemIndex);
+			// file << "\t\"" << index << "\":{\"data\":\"" << current->data_ << "\", \"randIndex\":" << randElemIndex << '}';
+			if (current->next_) file << ",\n";
+			current = current->next_;
+			++index;
+		}
+		file << "\n}";
+	}
+
+	std::string getFormattedString(const int index, std::string data, const int randElemIndex) {
+		return std::string("\t\"" + std::to_string(index) + "\":{\"data\":\"" + data + "\", \"randIndex\":" + std::to_string(randElemIndex) + '}');
+	}
+
+	void deserialize(std::ifstream& file)
+	{
+
+	}
 
 	void add(ListNode* listNode)
 	{
@@ -45,28 +77,29 @@ public:
 			tail_ = listNode;
 		}
 
-		// if it is the first element - we won't generate pointer to the random list element
-		if (count_ == 1)
+
+	}
+
+	void fillRands() 
+	{
+		for (ListNode* currentNode = head_; currentNode; currentNode = currentNode->next_)
 		{
-			return;
+			// make random the event of even creating a pointer to the random list element
+			bool needRand = rand() % 2;
+			if (!needRand) continue;
+			// make random pointer to the existing list element
+			const auto index = rand() % count_;
+
+			// iterate to this element
+			ListNode* current = head_;
+			for (int i = 0; i < index; ++i)
+			{
+				current = current->next_;
+			}
+
+			// assign it to the new element rand_ pointer
+			currentNode->rand_ = current;
 		}
-
-		// make random the event of even creating a pointer to the random list element
-		bool fillRand = rand() % 2;
-		if (!fillRand) return;
-
-		// make random pointer to the existing list element
-		const auto index = rand() % count_;
-
-		// iterate to this element
-		ListNode* current = head_;
-		for (int i = 0; i < index; ++i)
-		{
-			current = current->next_;
-		}
-
-		// assign it to the new element rand_ pointer
-		tail_->rand_ = current;
 	}
 
 	// for debugging
@@ -81,6 +114,17 @@ public:
 			current = current->next_;
 			++counter;
 		}
+	}
+
+	~ListRand() 
+	{
+		ListNode* current = head_->next_;
+		while (current)
+		{
+			delete current->prev_;
+			current = current->next_;
+		}
+		delete tail_;
 	}
 };
 
@@ -105,8 +149,17 @@ int main()
 	randomList->add(new ListNode("tenth"));
 	randomList->add(new ListNode("eleventh"));
 
+	randomList->fillRands();
+
 	randomList->print();
 
-	// std::ofstream outf("Serialized_list.txt");
-	// std::ifstream inf("Deserialized_list.txt");
+	std::ofstream outf("Serialized_list.txt");
+	randomList->serialize(outf);
+
+	// std::ifstream inf("Serialized_list.txt");
+	// randomList->deserialize(inf);
+
+	delete randomList;
+
+
 }
